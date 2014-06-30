20053b67b4e3e46d9ed870e295636aebc134553a
dtaniwaki <daisuketaniwaki@gmail.com>
2014-06-30 17:04:08 +0900

    Refactoring

--- lib/mandriller/settings_methods.rb
+++ lib/mandriller/settings_methods.rb
@@ -13,24 +13,50 @@ module Mandriller
           arg_s = "v"
         end
 
+        getter = options[:getter]
+
         keys.flatten.each do |key|
           class_eval <<-EOS
           class_attribute :mandrill_#{key}
-          def self.set_#{key}(#{arg_s})
+          def self.set_mandrill_setting_#{key}(#{arg_s})
             self.mandrill_#{key} = v
           end
-          private_class_method :set_#{key}
-          def set_#{key}(#{arg_s})
+          private_class_method :set_mandrill_setting_#{key}
+          self.singleton_class.send :alias_method, :set_#{key}, :set_mandrill_setting_#{key}
+          def set_mandrill_setting_#{key}(#{arg_s})
             @mandrill_#{key} = v
           end
-          private :set_#{key}
+          private :set_mandrill_setting_#{key}
+          alias_method :set_#{key}, :set_mandrill_setting_#{key}
           EOS
+
+          method_name = "get_mandrill_setting_#{key}"
+          define_method method_name do
+            v = get_mandrill_setting_value(key)
+            if getter
+              getter.call(v)
+            else
+              v
+            end
+          end
+          private method_name
         end
       end
     end
 
-    def get_mandrill_setting(key)
+    def get_mandrill_setting_value(key)
       instance_variable_defined?("@mandrill_#{key}") ? instance_variable_get("@mandrill_#{key}") : __send__("mandrill_#{key}")
+    end
+    private :get_mandrill_setting_value
+
+    def is_mandrill_setting_defined?(key)
+      v = get_mandrill_setting_value(key)
+      !v.nil?
+    end
+    private :is_mandrill_setting_defined?
+
+    def get_mandrill_setting(key)
+      __send__ "get_mandrill_setting_#{key}"
     end
     private :get_mandrill_setting
   end
--- spec/mandriller/settings_methods_spec.rb
+++ spec/mandriller/settings_methods_spec.rb
@@ -13,58 +13,109 @@ describe Mandriller::SettingsMethods do
     klass
   end
 
-  it "defines the methods" do
-    expect(subject.private_methods).to include(:set_foo)
-    expect(subject).to respond_to(:mandrill_foo)
-    expect(subject.private_instance_methods).to include(:set_foo)
+  describe "::define_settings_methods" do
+    it "defines the methods" do
+      expect(subject.private_methods).to include(:set_mandrill_setting_foo)
+      expect(subject.private_methods).to include(:set_foo)
+      expect(subject).to respond_to(:mandrill_foo)
+      expect(subject.private_instance_methods).to include(:set_mandrill_setting_foo)
+      expect(subject.private_instance_methods).to include(:set_foo)
+      expect(subject.private_instance_methods).to include(:get_mandrill_setting_foo)
 
-    instance = subject.new
-    expect(instance.instance_variable_defined?("@mandrill_foo")).to eq(false)
-  end
-  it "sets the instance variable" do
-    instance = subject.new
-    instance.send :set_foo, 'foo'
-    expect(instance.instance_variable_get("@mandrill_foo")).to eq('foo')
-  end
-  it "sets the class attribute" do
-    subject.send :set_foo, 'foo'
-    expect(subject.mandrill_foo).to eq('foo')
-  end
-
-  context "with default option" do
-    subject do
-      klass.class_eval do
-        define_settings_methods :foo, default: 'bar'
-      end
-      klass
-    end
-    it "sets the instance variable with default value" do
       instance = subject.new
-      instance.send :set_foo
-      expect(instance.instance_variable_get("@mandrill_foo")).to eq('bar')
+      expect(instance.instance_variable_defined?("@mandrill_foo")).to eq(false)
     end
-    it "sets the class attribute with default value" do
-      subject.send :set_foo
-      expect(subject.mandrill_foo).to eq('bar')
+
+    it "sets the instance variable" do
+      instance = subject.new
+      instance.send :set_mandrill_setting_foo, 'foo'
+      expect(instance.instance_variable_get("@mandrill_foo")).to eq('foo')
+    end
+    it "sets the class attribute" do
+      subject.send :set_mandrill_setting_foo, 'foo'
+      expect(subject.mandrill_foo).to eq('foo')
+    end
+    it "gets the value as is" do
+      instance = subject.new
+      instance.send :set_mandrill_setting_foo, 'foo'
+      expect(instance.send(:get_mandrill_setting_foo)).to eq('foo')
+    end
+
+    context "with default option" do
+      subject do
+        klass.class_eval do
+          define_settings_methods :foo, default: 'bar'
+        end
+        klass
+      end
+      it "sets the instance variable with default value" do
+        instance = subject.new
+        instance.send :set_mandrill_setting_foo
+        expect(instance.instance_variable_get("@mandrill_foo")).to eq('bar')
+      end
+      it "sets the class attribute with default value" do
+        subject.send :set_mandrill_setting_foo
+        expect(subject.mandrill_foo).to eq('bar')
+      end
+    end
+
+    context "with getter option" do
+      subject do
+        klass.class_eval do
+          define_settings_methods :foo, getter: lambda { |v| v.to_sym }
+        end
+        klass
+      end
+      it "gets the value as is" do
+        instance = subject.new
+        instance.send :set_mandrill_setting_foo, 'foo'
+        expect(instance.send(:get_mandrill_setting_foo)).to eq(:foo)
+      end
+    end
+  end
+
+  describe "#get_mandrill_setting_value" do
+    it "returns the value set globally" do
+      subject.send :set_mandrill_setting_foo, 1
+      instance = subject.new
+      expect(instance.send(:get_mandrill_setting_value, :foo)).to eq(1)
+    end
+    it "returns the value set locally" do
+      instance = subject.new
+      instance.send :set_mandrill_setting_foo, 2
+      expect(instance.send(:get_mandrill_setting_value, :foo)).to eq(2)
+    end
+    it "returns the value set locally over the one set globally" do
+      subject.send :set_mandrill_setting_foo, 1
+      instance = subject.new
+      instance.send :set_mandrill_setting_foo, 2
+      expect(instance.send(:get_mandrill_setting_value, :foo)).to eq(2)
+    end
+  end
+
+  describe "#is_mandrill_setting_defined?" do
+    it "returns true for the value set globally" do
+      subject.send :set_mandrill_setting_foo, 1
+      instance = subject.new
+      expect(instance.send(:is_mandrill_setting_defined?, :foo)).to eq(true)
+    end
+    it "returns true for the value set localy" do
+      instance = subject.new
+      instance.send :set_mandrill_setting_foo, 2
+      expect(instance.send(:is_mandrill_setting_defined?, :foo)).to eq(true)
+    end
+    it "returns false for non set value" do
+      instance = subject.new
+      expect(instance.send(:is_mandrill_setting_defined?, :foo)).to eq(false)
     end
   end
 
   describe "#get_mandrill_setting" do
-    it "returns class_attribute" do
-      subject.send :set_foo, 1
+    it "delegates to the setting method" do
       instance = subject.new
-      expect(instance.send(:get_mandrill_setting, :foo)).to eq(1)
-    end
-    it "returns instance_variable" do
-      instance = subject.new
-      instance.send :set_foo, 2
-      expect(instance.send(:get_mandrill_setting, :foo)).to eq(2)
-    end
-    it "returns instance_variable over class_attribute" do
-      subject.send :set_foo, 1
-      instance = subject.new
-      instance.send :set_foo, 2
-      expect(instance.send(:get_mandrill_setting, :foo)).to eq(2)
+      ret = double
+      expect(instance).to receive(:get_mandrill_setting_foo).and_return(ret)
+      expect(instance.send(:get_mandrill_setting, :foo)).to eq(ret)
     end
   end
 end
